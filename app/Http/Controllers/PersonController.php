8785f8eb185aea4e321002ea02ee1f17716ad178
Sergio <seranmoreno500@gmail.com>
2019-12-03 13:08:03 +0100

    Pelis eliminadas
    asd

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -18,7 +18,7 @@ class PersonController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('show');    
+        $this->middleware('auth')->except('index', 'show');    
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////
